d44a9724aae81b968418d46e6ff82cea01e20f4f
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2023-10-07 14:11:13 -0300

    Fix algo_w entrypoint

--- algo_w/algo_w.ml
+++ algo_w/algo_w.ml
@@ -1,11 +1,18 @@
 module Expr = Expr
 module Infer = Infer
 
-let parse_ty _str = failwith "Not implemented"
 
-let parse_expr _str = failwith "Not implemented"
+let parse_ty str =
+  let lexbuf = Lexing.from_string str in
+  Parser.qual_ty_forall_eof Lexer.token lexbuf
 
-let parse_qual_pred _str = failwith "Not implemented"
+let parse_expr str =
+  let lexbuf = Lexing.from_string str in
+  Parser.expr_eof Lexer.token lexbuf
+
+let parse_qual_pred str =
+  let lexbuf = Lexing.from_string str in
+  Parser.qual_pred_eof Lexer.token lexbuf
 
 let infer_ty ?(env = Infer.Env.empty) expr =
   try Ok (Infer.infer env expr) with
